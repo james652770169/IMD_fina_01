b6991bcbe5848ccdb1499b77e2a798a101ed11d1
james652770169 <126155289+james652770169@users.noreply.github.com>
2026-01-05 20:39:23 +0800

    Update 05_第五章 streamlit UI設計.py

--- pages/05_第五章 streamlit UI設計.py
+++ pages/05_第五章 streamlit UI設計.py
@@ -16,7 +16,8 @@ code = """
             """
 st.code(code, language="python")
 
-st.image("Picture/第4章01.png")
+st.image("picture/第4章01.png")
 
 st.markdown('### 步驟3：開啟"[streamlit](https://cheat-sheet.streamlit.app/)"官網的程式庫，從中可獲的各種程式以供網頁書寫') 
+
 
